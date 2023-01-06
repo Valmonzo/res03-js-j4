45426f3043681c25394c5514bb24a9cd361670e0
Valmonzo <valmontpehautpietri@ip-172-31-29-136.eu-west-3.compute.internal>
2023-01-06 11:57:34 +0000

    Exo du matin

--- exercices/exo1/exo1.js
+++ exercices/exo1/exo1.js
@@ -45,13 +45,54 @@ function exercice4() {
             secret: "Iron Man"
         }
     ];
+
+    let btn = document.getElementById("btn-add-hero");
+    let tab = document.querySelector("#ex4 tbody");
+    let count = 0
+
+    btn.addEventListener("click", function() {
+
+            let tr = document.createElement("tr");
+            let tdName = document.createElement("td");
+            let tdSecret = document.createElement("td");
+
+            let txtName = document.createTextNode(heroes[count].name);
+            let txtSecret = document.createTextNode(heroes[count].secret);
+
+            tdName.appendChild(txtName);
+            tdSecret.appendChild(txtSecret);
+            tr.appendChild(tdName);
+            tr.appendChild(tdSecret);
+
+            tab.appendChild(tr);
+            count++;
+
+
+    });
 }
 
 function exercice5() {
+let blue = document.getElementById("blue");
+let red = document.getElementById("red");
+let green = document.getElementById("green");
+let btnColor = document.getElementById("btn-turn")
 
+btnColor.addEventListener("click", function() {
+    blue.id = "red";
+    red.id = "green"
+    green.id = "blue";
+})
 }
 
 function exercice6() {
+
+    let btnBall = document.getElementById("btn-roll");
+
+    btnBall.addEventListener("click", function() {
+        let ball = document.getElementById("ball");
+        ball.classList.toggle("animation");
+    })
+
 
 }
 
@@ -60,4 +101,7 @@ window.addEventListener("DOMContentLoaded", function(){
     exercice1();
     exercice2();
     exercice3();
+    exercice4();
+    exercice5();
+    exercice6();
 });
